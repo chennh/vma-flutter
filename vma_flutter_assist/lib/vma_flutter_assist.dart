2185ebb57de79df82dd1393a2c3f81a04094672c
陈乃华 <810422646@qq.com>
2020-05-09 11:01:58 +0800

    uiRouter

--- vma_flutter_assist/lib/vma_flutter_assist.dart
+++ vma_flutter_assist/lib/vma_flutter_assist.dart
@@ -1,6 +1,8 @@
 library vma_flutter_assist;
 
+export 'src/assist.dart';
 export 'src/listener/listener.dart';
 export 'src/model/model.dart';
+export 'src/plugin/index.dart';
 export 'src/router/index.dart';
 export 'src/router/uiRouter.dart';
