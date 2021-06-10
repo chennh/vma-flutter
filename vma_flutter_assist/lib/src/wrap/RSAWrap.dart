335802e5e4cf176a0e30b0756a7fc6ff25143088
陈乃华 <810422646@qq.com>
2021-06-10 17:25:52 +0800

    no message

---
+++ vma_flutter_assist/lib/src/wrap/RSAWrap.dart
@@ -0,0 +1,21 @@
+import 'package:encrypt/encrypt.dart';
+import 'package:pointycastle/asymmetric/api.dart';
+
+class RSAWrap {
+  ///
+  /// 根据模和指数生成publicKey
+  ///
+  static RSAPublicKey getPublicKey(String modulus, String exponent) {
+    return RSAPublicKey(
+        BigInt.parse(modulus, radix: 16), BigInt.parse(exponent, radix: 16));
+  }
+
+  ///
+  /// 加密
+  ///
+  static String encrypt(String data, String modulus, String exponent) {
+    return Encrypter(RSA(publicKey: getPublicKey(modulus, exponent)))
+        .encrypt(data.split("").reversed.join())
+        .base16;
+  }
+}
