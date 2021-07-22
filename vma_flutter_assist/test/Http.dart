4babaf0e51e603bd487ce15983c902e5f943b6b1
陈乃华 <810422646@qq.com>
2021-07-22 20:38:01 +0800

    no message

--- vma_flutter_assist/test/Http.dart
+++ vma_flutter_assist/test/Http.dart
@@ -1,9 +1,11 @@
 import 'package:vma_flutter_assist/vma_flutter_assist.dart';
 import 'package:dio/dio.dart';
-import 'api/common/v1.0/AccountApi.dart';
+import 'api/common/v1.0/accountApi.dart';
+import 'api/common/v1.0/qiniuOssApi.dart';
 import 'api/common/v1.0/definitions/EncryptionResp.dart';
 import 'api/common/v1.0/definitions/AccountLoginReq.dart';
 import 'api/common/v1.0/definitions/AccountLoginResp.dart';
+import 'api/common/v1.0/definitions/QiniuTokenBO.dart';
 
 void main() async {
   Map<String, String> map = {'token': ''};
@@ -21,22 +23,27 @@ void main() async {
   String account = 'xmls10010';
   String password = '123456';
 
-  print('getEncryption');
-  EncryptionResp encryptionResp =
-      (await AccountApi.getEncryption()).data as EncryptionResp;
-  String encryptPassword = RSAWrap.encrypt(
-      password, encryptionResp.modulus as String, encryptionResp.exponent as String);
+  // 登录
+//  print('getEncryption');
+//  EncryptionResp encryptionResp =
+//      (await AccountApi.getEncryption()).data as EncryptionResp;
+//  String encryptPassword = RSAWrap.encrypt(
+//      password, encryptionResp.modulus as String, encryptionResp.exponent as String);
+//
+//  print('login');
+//  AccountLoginResp loginResp = (await AccountApi.login(AccountLoginReq(
+//          account: account,
+//          password: encryptPassword,
+//          randomIndex: encryptionResp.randomIndex)))
+//      .data as AccountLoginResp;
+//  map['token'] = loginResp.macKey as String;
+//
+//  print('getCurrent: ' + (map['token'] as String));
+//  AccountLoginResp loginResp2 =
+//      (await AccountApi.getCurrent()).data as AccountLoginResp;
+//  print(loginResp2);
 
-  print('login');
-  AccountLoginResp loginResp = (await AccountApi.login(AccountLoginReq(
-          account: account,
-          password: encryptPassword,
-          randomIndex: encryptionResp.randomIndex)))
-      .data as AccountLoginResp;
-  map['token'] = loginResp.macKey as String;
-
-  print('getCurrent: ' + (map['token'] as String));
-  AccountLoginResp loginResp2 =
-      (await AccountApi.getCurrent()).data as AccountLoginResp;
-  print(loginResp2);
+  // 取七牛token，是个内嵌对象
+  QiniuTokenBO qiniuTokenBO = (await QiniuOssApi.getQiNiuAddress()).data as QiniuTokenBO;
+  print(qiniuTokenBO);
 }
